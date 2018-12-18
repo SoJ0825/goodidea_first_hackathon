e7258240e3563261321add9157f90e0a124d784b
SOJ <takayashi0825@gmail.com>
2018-12-19 01:59:12 +0800

    [Bug]Validate request for function getAchievement

--- app/Http/Controllers/AchievementController.php
+++ app/Http/Controllers/AchievementController.php
@@ -4,11 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Achievement;
+use Illuminate\Support\Facades\Validator;
 
 class AchievementController extends Controller {
 
     public function getAchievement(Request $request)
     {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'achievement' => ['required', 'string', 'regex:(allWin|loss10Time|luckyAce|poorYou|lovelyQueen)'],
+                'api_token' => 'required|string|max:32',
+                'bool' => 'required|bool',
+            ],
+            [
+                'achievement.regex' => 'The :attribute format is allWin , loss10Time, luckyAce, poorYou or lovelyQueen.'
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response(['result' => 'false', 'response' => $validator->errors()->first()], 400);
+        }
             $achievement = Achievement::select('user_id', $request['achievement'])
                 ->where('user_id', session('id'))
 //                ->where($request['achievement'], $request['achievement'])
