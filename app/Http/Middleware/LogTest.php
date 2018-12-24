be504b2c05dba450fd307008ce508442cc4ee2da
SOJ <takayashi0825@gmail.com>
2018-12-24 15:15:40 +0800

    [Bug]add request header into record log

--- app/Http/Middleware/LogTest.php
+++ app/Http/Middleware/LogTest.php
@@ -5,23 +5,27 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Log;
 
-class LogTest
-{
+class LogTest {
+
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        Log::info($request->method(), $request->input(), $request->path());
+        Log::info('METHOD : ' . $request->method());
+        Log::info('PATH : ' . $request->path());
+        Log::info($request->header());
+        Log::info($request->input());
 
         $response = $next($request);
 //        dd('mi');
         Log::info($response->getContent());
         Log::info('--------------------------------------');
+
         return $response;
 //        return $next($request);
     }
